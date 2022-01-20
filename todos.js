b0992e51ca19465f8b42c5341b0e0d3fff2c8879
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2022-01-21 01:21:06 +0330

    created a function and a loop that can get information from local storage and update the list

--- todos.js
+++ todos.js
@@ -1,8 +1,8 @@
-// get todos from localstorage
+// get todos from localstorage(it gives us a todos but its an string and we should convert it to an onject)
 let todos = localStorage.getItem("todos")
 //try parse data or null
 try{
-    todos = JSON.parse(todos)
+    todos = JSON.parse(todos) //we use JSON.parse to convert todos from string to object
     todos = todos.length ? todos : null
 } catch(e) {
     todos = null
@@ -17,3 +17,30 @@ if (!todos) {
     ]
     localStorage.setItem("todos",JSON.stringify(todos))
 }
+// func to create or update todos list in ui
+function createTodos(todos){
+    let todosList = document.querySelector("#todos-list")
+    todosList.innerHTML = ""
+
+    // create list tag for each todo
+    todos.forEach(todo => {
+        let li = document.createElement("li")
+        li.className = "list-group-item"
+        let content = document.createElement("span")
+        content.textContent = todo
+        let deleteBtn = document.createElement("img")
+        deleteBtn.src = "media/delete.png"
+        deleteBtn.alt = "delete icon"
+        deleteBtn.className = "float-right"
+
+        //append content and deleteBtn to li
+        li.append(content)
+        li.append(deleteBtn)
+        
+        // append li to todosList
+        todosList.append(li)
+        
+    });
+}
+
+createTodos(todos)
